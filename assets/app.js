b9f582faf706a81209fbd17075d6019bd0d02071
ksatter <kberis@gmail.com>
2017-09-22 19:32:26 -0400

    "enhanced search function"

--- assets/app.js
+++ assets/app.js
@@ -1,20 +1,49 @@
-$(document).ready(function() {
-    // create array of topics
-    var topics = ["Zoe", "Wash", "Mal"];
-    // create button for each topic
-    function createButtons(){
-        // empty button dic=v
-        $("#buttons").empty();
-        // for each item in topic array
-        for (var i = 0; i < topics.length; i++) {
-            var currentTopic = topics[i];
-            // create button
-            var newTopic = $("<button>", {class: "btn btn-primary topic"});
-            newTopic.attr('data-query', currentTopic);
-            newTopic.text(currentTopic);
-            $("#buttons").append(newTopic);
-        }
+// create array of topics
+var topics = ["Zoe", "Wash", "Mal"];
+
+// create button for each topic
+function createButtons(){
+    // empty button dic=v
+    $("#buttons").empty();
+    // for each item in topic array
+    for (var i = 0; i < topics.length; i++) {
+        var currentTopic = topics[i];
+        // create button
+        var newTopic = $("<button>", {class: "btn btn-primary topic"});
+        newTopic.attr('data-query', currentTopic);
+        newTopic.attr('id', 'button-'+ i);
+        newTopic.text(currentTopic);
+        $("#buttons").append(newTopic);
     }
+}
+
+//convert string to title case. Tried writing this myself, but kept running in to roadblocks.
+/*
+* To Title Case 2.1 – http://individed.com/code/to-title-case/
+* Copyright © 2008–2013 David Gouch. Licensed under the MIT License.
+*/
+String.prototype.toTitleCase = function(){
+    var smallWords = /^(a|an|and|as|at|but|by|en|for|if|in|nor|of|on|or|per|the|to|vs?\.?|via)$/i;
+
+    return this.replace(/[A-Za-z0-9\u00C0-\u00FF]+[^\s-]*/g, function(match, index, title){
+        if (index > 0 && index + match.length !== title.length &&
+            match.search(smallWords) > -1 && title.charAt(index - 2) !== ":" &&
+            (title.charAt(index + match.length) !== '-' || title.charAt(index - 1) === '-') &&
+            title.charAt(index - 1).search(/[^\s-]/) < 0) {
+            return match.toLowerCase();
+        }
+
+        if (match.substr(1).search(/[A-Z]|\../) > -1) {
+            return match;
+        }
+
+        return match.charAt(0).toUpperCase() + match.substr(1);
+    });
+};
+
+$(document).ready(function() {
+
+    createButtons();
     // when button is pushed, query giphy API for results and display still images
     $(document).on("click", ".topic", function() {
         //create query url
@@ -37,7 +66,6 @@ $(document).ready(function() {
                 if (used.indexOf(random) < 0) {
                     used.push(random);
                     i++;
-                    console.log(random);
                     //create card for image
                     var newCard = $("<div>", {class: "card"});
                     //create image
@@ -74,25 +102,34 @@ $(document).ready(function() {
             $(this).attr("data-state", "still");
         }
     });
-    //add topic to list
+    //add topic to list from search bar
     $(document).on("click", "#new-topic", function() {
+        event.preventDefault();
         // retrieve input from text field
-        var newTopic = $('#form-input').val();
-        // clear text field
-        $('#form-input').val("");
-
-        topics.push(newTopic);
-        createButtons()
-        //check that input is valid
+        var newTopic = $(".form-control").val().toTitleCase();
+            console.log(newTopic);
+        // verify that there is a search term and it is not already in topics
+        if (newTopic && topics.indexOf(newTopic) < 0) {
+            //add search to topics
+            topics.push(newTopic);
+            // re-write buttons
+            createButtons();
+            // display results for search term
+            $("#buttons button:last-child").click();
+        }
+        // if search term already in topics list, display results
+        else if (topics.indexOf(newTopic) >= 0) {
+            $("#button-" + topics.indexOf(newTopic)).click();
+        }
+        //empty search input
+        $(".form-control").val("")
     });
-    $(document).on("keyup", "#form-input",function () {
+    //enable searching by hitting enter key
+    $(document).on("keyup", "#form-input", function () {
+        // if enter key hit
         if (event.which === 13) {
+            //run search
             $("#new-topic").click();
-            $("#buttons button:last-child").click()
         }
     });
-
-
-    createButtons();
-    console.log(topics)
-})
+});
